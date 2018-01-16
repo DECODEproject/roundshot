9b1e86acc6cbdc5e3d25de7d3f0451509c3f7c41
parazyd <parazyd@dyne.org>
2018-01-16 14:07:50 +0100

    Update ROOTFS_URL to the decode tarball.

--- squashfs/config.mk
+++ squashfs/config.mk
@@ -1,3 +1,3 @@
 # See LICENSE file for copyight and license details
 
-ROOTFS_URL ?= https://pub.parazyd.cf/tmp/rootfs.tar.gz
+ROOTFS_URL ?= https://sdk.dyne.org/decode/decode-0.1-armhf-sunxi.tar.gz
