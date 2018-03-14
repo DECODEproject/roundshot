02780bf51b641108758b866b1ff26179a7d2bcce
parazyd <parazyd@dyne.org>
2018-03-14 14:40:06 +0100

    Update some versions and links.

--- squashfs/config.mk
+++ squashfs/config.mk
@@ -1,3 +1,3 @@
 # See LICENSE file for copyight and license details
 
-ROOTFS_URL ?= https://sdk.dyne.org/decode/decode-0.1-armhf.tar.gz
+ROOTFS_URL ?= https://sdk.dyne.org:4443/job/decode-rootfs-tarball-armhf/lastSuccessfulBuild/artifact/arm-sdk/dist/decode-1.0.0-armhf.tar.gz
--- u-boot/config.mk
+++ u-boot/config.mk
@@ -1,6 +1,6 @@
 # See LICENSE file for copyright and license details
 
-U_BOOT_VERSION = v2018.01-rc2
+U_BOOT_VERSION = v2018.03
 U_BOOT_URL = git://git.denx.de/u-boot.git
 
 ARM_DEFCONFIG = A20-OLinuXino-Lime2_defconfig
