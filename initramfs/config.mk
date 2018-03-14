45e2a8142988113021ecb3e0ae73eebcff8c7520
parazyd <parazyd@dyne.org>
2018-03-14 15:57:07 +0100

    Install static btrfs in the initramfs.

--- initramfs/config.mk
+++ initramfs/config.mk
@@ -8,5 +8,9 @@ GNUPG = 1
 GNUPG_VERSION = 2.2.4-1
 GNUPG_URL = https://deb.debian.org/debian/pool/main/g/gnupg2/gpgv-static_$(GNUPG_VERSION)_armhf.deb
 
+BTRFS = 1
+BTRFS_URL = https://sdk.dyne.org:4443/job/btrfs-progs/architecture=armhf/lastSuccessfulBuild/artifact/btrfs.static
+BTRFSMKFS_URL = https://sdk.dyne.org:4443/job/btrfs-progs/architecture=armhf/lastSuccessfulBuild/artifact/mkfs.btrfs.static
+
 TC_PATH = $(PWD)/../toolchain/arm-linux-musleabihf
 TC_NAME = arm-linux-musleabihf-
